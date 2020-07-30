b15de247b83e60ed5997c10be22c1feafc52a439
Rafael Bená Cineis <rafael.cineis@hotmail.com>
2020-07-30 00:39:42 -0300

    Create Text component

---
+++ app/components/Text/index.js
@@ -0,0 +1,52 @@
+/**
+ *
+ * Text
+ *
+ */
+
+import styled, { css } from 'styled-components'
+
+const BoldText = css`
+  font-weight: bold;
+`
+
+const SemiBoldText = css`
+  font-weight: 600;
+`
+
+const LightText = css`
+  font-weight: 300;
+`
+
+const BigText = css`
+  font-size: 20px;
+`
+
+const MediumText = css`
+  font-size: 18px;
+`
+
+const SmallText = css`
+  font-size: 12px;
+`
+
+const UppercaseText = css`
+  text-transform: uppercase;
+`
+
+const Text = styled.span`
+  margin: 0;
+
+  font-size: 1em;
+  color: #000000;
+
+  ${props => props.big && BigText}
+  ${props => props.medium && MediumText}
+  ${props => props.small && SmallText}
+  ${props => props.bold && BoldText}
+  ${props => props.semiBold && SemiBoldText}
+  ${props => props.light && LightText}
+  ${props => props.uppercase && UppercaseText}
+`
+
+export default Text
